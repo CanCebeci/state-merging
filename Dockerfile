a36209f14e5648b20ec96165c2717cae9d240278
Can Cebeci <can.cebeci@epfl.ch>
2021-11-20 15:09:52 +0100

    add 2.9

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ COPY klee /klee
 # copy downloads (instead of using wget in container -- SLL issues)
 COPY downloads/llvm-gcc-4.2-2.6-x86_64-linux /llvm-gcc-4.2-2.6-x86_64-linux
 COPY downloads/llvm-2.6 /llvm-2.6
+COPY downloads/llvm-gcc4.2-2.9-x86_64-linux /llvm-gcc4.2-2.9-x86_64-linux
+COPY downloads/llvm-2.9 /llvm-2.9
 
 
 RUN chmod u+x build_dependencies.sh && ./build_dependencies.sh
--- build_dependencies.sh
+++ build_dependencies.sh
@@ -7,7 +7,7 @@ apt-get install -y wget gcc g++ build-essential subversion libz-dev flex bison l
 
 echo "check_certificate = off" >> ~/.wgetrc
 
-# get gcc frontend binaries 
+# get gcc frontend binaries (2.6)
 echo "export PATH=/llvm-gcc-4.2-2.6-x86_64-linux/bin:$PATH" >> ~/.bashrc
 . ~/.bashrc
 
@@ -17,6 +17,19 @@ cd llvm-2.6
 make -j96
 echo "export PATH=/llvm-2.6/Release/bin:$PATH" >> ~/.bashrc
 . ~/.bashrc
+cd ..
+
+# get gcc frontend binaries (2.9)
+echo "export PATH=/llvm-gcc-4.2-2.9-x86_64-linux/bin:$PATH" >> ~/.bashrc
+. ~/.bashrc
+
+# get llvm 2.9
+cd llvm-2.9
+./configure --enable-optimized --enable-assertions
+make -j96
+echo "export PATH=/llvm-2.9/Release+Asserts/bin:$PATH" >> ~/.bashrc
+. ~/.bashrc
+
 
 # these are required to build klee-uclibc
 ln -s /usr/include/asm-generic /usr/include/asm
